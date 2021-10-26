f1a27bb83fb29da81cdcbd9403e568ec1a60de30
EC2 Default User <ec2-user@ip-10-40-77-107.us-east-2.compute.internal>
2021-10-26 06:59:12 +0000

    updated

---
+++ step_v2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sudo curl --silent --location -o /usr/local/bin/kubectl \
+   https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+
+sudo chmod +x /usr/local/bin/kubectl
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+rm -f awscliv2.zip
+rm -rf aws
+
+sudo yum -y install jq gettext bash-completion moreutils
+
+echo 'yq() {
+  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
+}' | tee -a ~/.bashrc && source ~/.bashrc
+
+
+for command in kubectl jq envsubst aws
+  do
+    which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
+  done
+
+kubectl completion bash >>  ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+
+echo 'export LBC_VERSION="v2.2.0"' >>  ~/.bash_profile
+.  ~/.bash_profile
+
+
