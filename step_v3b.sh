13971b38edcc8aba0a1230b20af31c9d696ab403
EC2 Default User <ec2-user@ip-10-40-70-250.us-east-2.compute.internal>
2021-10-28 18:39:46 +0000

    updated

--- step_v3b.sh
+++ step_v3b.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EGION1="us-east-2"
+REGION1="us-east-2"
 REGION2="us-west-2"
 
 export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
