da31d38f9c916a0073283527413c4a3610f9b9ac
EC2 Default User <ec2-user@ip-10-40-70-250.us-east-2.compute.internal>
2021-10-28 18:36:15 +0000

    updated

--- step_v3a.sh
+++ step_v3a.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-iamrole=$(aws cloudformation describe-stacks --stack-name EKSGDB1 --query 'Stacks[].Outputs[?(OutputKey == `Cloud9Role`)][].{OutputValue:OutputValue}'   --output text)
+REGION1="us-east-2"
+REGION2="us-west-2"
+
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || echo AWS_REGION is not set
+
+iamrole=$(aws cloudformation describe-stacks --stack-name EKSGDB1 --query 'Stacks[].Outputs[?(OutputKey == `Cloud9Role`)][].{OutputValue:OutputValue}' --region $REGION1  --output text)
 vpcid=$(aws cloudformation describe-stacks --stack-name EKSGDB1 --query 'Stacks[].Outputs[?(OutputKey == `VPC`)][].{OutputValue:OutputValue}' --output text)
 instid=$(aws ec2 describe-instances --filters Name=vpc-id,Values=${vpcid} --query 'Reservations[].Instances[].InstanceId' --output text)
 
-aws iam create-instance-profile --instance-profile-name cloud9InstanceProfile
-
-aws iam add-role-to-instance-profile --role-name ${iamrole} --instance-profile-name cloud9InstanceProfile
-
+if [[ "${AWS_REGION}" == "${REGION1}" ]]; then
+  aws iam create-instance-profile --instance-profile-name cloud9InstanceProfile
+  aws iam add-role-to-instance-profile --role-name ${iamrole} --instance-profile-name cloud9InstanceProfile
+  echo "Waiting for 60 seconds.."
+  sleep 60
+fi
 
