f61760fe7fb9c30e5d66a065eafaada4f29b6157
EC2 Default User <ec2-user@ip-10-40-70-250.us-east-2.compute.internal>
2021-10-29 02:30:12 +0000

    updated

---
+++ step_v6.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+helm version --short
+
+eksctl utils associate-iam-oidc-provider \
+  --region ${AWS_REGION} \
+  --cluster eksgdbclu \
+  --approve
+
+curl -o iam_policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.3.0/docs/install/iam_policy.json
+aws iam create-policy \
+  --policy-name AWSLoadBalancerControllerIAMPolicy \
+  --policy-document file://iam_policy.json
+
+eksctl create iamserviceaccount \
+  --cluster eksgdbclu \
+  --namespace kube-system \
+  --name aws-load-balancer-controller \
+  --attach-policy-arn arn:aws:iam::${ACCOUNT_ID}:policy/AWSLoadBalancerControllerIAMPolicy \
+  --override-existing-serviceaccounts \
+  --approve
+
+kubectl apply -k github.com/aws/eks-charts/stable/aws-load-balancer-controller/crds?ref=master
+
+kubectl get crd
+
+helm repo add eks https://aws.github.io/eks-charts
+
+helm upgrade -i aws-load-balancer-controller \
+    eks/aws-load-balancer-controller \
+    -n kube-system \
+    --set clusterName=eksgdbclu \
+    --set serviceAccount.create=false \
+    --set serviceAccount.name=aws-load-balancer-controller \
+    --set image.tag="${LBC_VERSION}"
+
+kubectl -n kube-system rollout status deployment aws-load-balancer-controller
+
+
