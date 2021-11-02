40a2c06e814744ec5b17519df4a3f305310d16d5
EC2 Default User <ec2-user@ip-10-40-70-250.us-east-2.compute.internal>
2021-11-02 06:18:06 +0000

    updated

--- step_v6.sh
+++ step_v6.sh
@@ -41,4 +41,8 @@ helm upgrade -i aws-load-balancer-controller \
 
 kubectl -n kube-system rollout status deployment aws-load-balancer-controller
 
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.5.0/components.yaml
+sleep 30
+kubectl get apiservice v1beta1.metrics.k8s.io -o json | jq '.status'
+
 
