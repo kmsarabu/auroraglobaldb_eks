dc0fa1d0af635f888e4dd9992c0dc034f17f0d50
ajrajkumar <aj_rajkumar@yahoo.com>
2022-10-15 02:27:36 +0000

    Updated

--- deploy_cft.sh
+++ deploy_cft.sh
@@ -19,38 +19,40 @@ function wait_for_stack_to_complete() {
 ## MAIN
 aws cloudformation create-stack --stack-name EKSGDB1 \
          --disable-rollback \
-	 --template-body file://aurora_vpc_region.json \
+	 --template-body file://aurora_vpc_region.yaml \
 	 --tags Key=Environment,Value=EKSGDB \
 	 --timeout-in-minutes=30 --region ${REGION1} \
 	 --capabilities "CAPABILITY_IAM" \
 	 --parameters ParameterKey=DeployAurora,ParameterValue=true ParameterKey=ClassB,ParameterValue=40
 if [[ $? -ne 0 ]]; then
-  echo "Stack aurora_vpc_region.json, EKSGDB1 failed to deploy on region ${REGION1}, Please check/fix the error and retry"
+  echo "Stack aurora_vpc_region.yaml, EKSGDB1 failed to deploy on region ${REGION1}, Please check/fix the error and retry"
   exit 1
 fi
-wait_for_stack_to_complete "EKSGDB1" "${REGION1}"
 
 aws cloudformation create-stack --stack-name EKSGDB1 \
-	 --template-body file://aurora_vpc_region.json \
+	 --template-body file://aurora_vpc_region.yaml \
 	  --tags Key=Environment,Value=EKSGDB \
 	   --timeout-in-minutes=30 --region ${REGION2} \
 	    --capabilities "CAPABILITY_IAM" \
 	     --parameters ParameterKey=DeployAurora,ParameterValue=false ParameterKey=ClassB,ParameterValue=50
 if [[ $? -ne 0 ]]; then
-	echo "Stack aurora_vpc_region.json, EKSGDB1 failed to deploy on region ${REGION2}, Please check/fix the error and retry"
+	echo "Stack aurora_vpc_region.yaml, EKSGDB1 failed to deploy on region ${REGION2}, Please check/fix the error and retry"
   exit 1
 fi
+
+wait_for_stack_to_complete "EKSGDB1" "${REGION1}"
 wait_for_stack_to_complete "EKSGDB1" "${REGION2}"
  
 aws cloudformation create-stack --stack-name EKSGDB2 \
-	 --template-body file://aurora_gdb.json \
+	 --template-body file://aurora_gdb.yaml \
 	  --tags Key=Environment,Value=EKSGDB \
 	   --timeout-in-minutes=30 --region ${REGION2}
 
 if [[ $? -ne 0 ]]; then
-  echo "Stack aurora_gdb.json, EKSGDB2 failed to deploy on region ${REGION1}, Please check/fix the error and retry"
+  echo "Stack aurora_gdb.yaml, EKSGDB2 failed to deploy on region ${REGION1}, Please check/fix the error and retry"
   exit 1
 fi
+
 wait_for_stack_to_complete "EKSGDB2" "${REGION2}"
 
 echo "Completed deloying the CloudFormation Stacks on regions us-east-2 and us-west-2"
--- install_utilities.sh
+++ install_utilities.sh
@@ -44,3 +44,21 @@ echo 'export LBC_VERSION="v2.2.0"' >>  ~/.bash_profile
 sudo amazon-linux-extras install postgresql13 -y
 sudo yum -y install postgresql-contrib postgresql-devel
 
+## Install Helm
+
+curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+helm version --short
+if [[ $? -ne 0 ]]; then
+ echo "Helm not installed, please fix and retry"
+ exit 1
+fi
+
+helm repo add stable https://charts.helm.sh/stable
+
+helm search repo stable
+
+helm completion bash >> ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+source <(helm completion bash)
