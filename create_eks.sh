87898f92ea1405a5ac0af8946a4dc2dd8fc1a8b4
EC2 Default User <ec2-user@ip-10-40-69-64.us-east-2.compute.internal>
2022-10-18 03:59:52 +0000

    UPdated

--- create_eks.sh
+++ create_eks.sh
@@ -1,8 +1,44 @@
 #!/bin/sh
 
+function print_line()
+{
+    echo "---------------------------------"
+}
+
+function chk_cloud9_permission()
+{
+    print_line
+    echo "Fixing the cloud9 permission"
+    print_line
+    aws sts get-caller-identity | grep ${INSTANCE_ROLE}  
+    if [ $? -ne 0 ] ; then
+	echo "Fixing the cloud9 permission"
+        environment_id=`aws ec2 describe-instances --region ${AWS_REGION} --instance-id $(curl -s http://169.254.169.254/latest/meta-data/instance-id) --query "Reservations[*].Instances[*].Tags[?Key=='aws:cloud9:environment'].Value" --output text`
+        aws cloud9 update-environment --environment-id ${environment_id} --region ${AWS_REGION} --managed-credentials-action DISABLE
+	sleep 10
+        ls -l $HOME/.aws/credentials > /dev/null 2>&1
+        if [ $? -eq 0 ] ; then
+             echo "!!! Credentials file exists"
+        else
+            echo "Credentials file does not exists"
+        fi
+	echo "After fixing the credentials. Current role"
+        aws sts get-caller-identity | grep ${INSTANCE_ROLE} > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "Permission set properly for the cloud9 environment"
+        else
+            echo "!!! Permission not set properly for the cloud9 environment"
+            aws sts get-caller-identity
+            exit 1
+        fi
+    fi
+}
 
 function create_eks_cluster()
 {
+    print_line
+    echo "Creatingn EKS cluster"
+    print_line
     typeset -i counter
     counter=0
     echo "aws cloudformation  create-stack --stack-name ${EKS_STACK_NAME} --parameters ParameterKey=VPC,ParameterValue=${vpcid} ParameterKey=SubnetAPrivate,ParameterValue=${subnetA} ParameterKey=SubnetBPrivate,ParameterValue=${subnetB} ParameterKey=SubnetCPrivate,ParameterValue=${subnetC} --template-body file://${EKS_CFN_FILE} --capabilities CAPABILITY_IAM"
@@ -28,23 +64,34 @@ function create_eks_cluster()
        echo "EKS cluster Stack creation failed with status ${STATUS}.. exiting"
        exit 1 
     fi
+    print_line
 }
 
 function update_config()
 {
-    aws eks update-kubeconfig --name ${EKS_CLUSTER_NAME}
+    print_line
+    echo "Updating kubectl config" 
+    print_line
+    aws eks update-kubeconfig --name ${EKS_CLUSTER_NAME} --region ${AWS_REGION}
+    print_line
 }
 
 function update_eks()
 {
+    print_line
     echo "Enabling clusters to use iam oidc"
+    print_line
     eksctl utils associate-iam-oidc-provider --cluster ${EKS_CLUSTER_NAME} --region ${AWS_REGION} --approve
+    print_line
 }
+
 
 function install_loadbalancer()
 {
-
+    print_line
     echo "Installing load balancer"
+    print_line
+
     eksctl create iamserviceaccount \
      --cluster=${EKS_CLUSTER_NAME} \
      --namespace=${EKS_NAMESPACE} \
@@ -53,9 +100,8 @@ function install_loadbalancer()
      --override-existing-serviceaccounts \
      --approve
 
-    helm repo add eks https://aws.github.io/eks-charts
-
     kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
+    helm repo add eks https://aws.github.io/eks-charts
 
     helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
      --set clusterName=${EKS_CLUSTER_NAME} \
@@ -70,6 +116,9 @@ function install_loadbalancer()
 function install_cluster_auto_scaler()
 {
 
+    print_line
+    echo "Installing Auto Scaler configuration" 
+    print_line
     mkdir ~/environment/cluster-autoscaler
 
     cat <<EoF > ~/environment/cluster-autoscaler/k8s-asg-policy.json
@@ -97,13 +146,17 @@ EoF
 
     eksctl create iamserviceaccount \
         --name cluster-autoscaler \
-        --namespace kube-system \
+        --namespace ${EKS_NAMESPACE} \
         --cluster ${EKS_CLUSTER_NAME} \
         --attach-policy-arn "arn:aws:iam::${AWS_ACCOUNT_ID}:policy/k8s-asg-policy" \
         --approve \
         --override-existing-serviceaccounts
 
-    kubectl apply -f https://www.eksworkshop.com/beginner/080_scaling/deploy_ca.files/cluster-autoscaler-autodiscover.yaml
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes/autoscaler/master/cluster-autoscaler/cloudprovider/aws/examples/cluster-autoscaler-autodiscover.yaml
+
+    kubectl -n kube-system \
+        annotate deployment.apps/cluster-autoscaler \
+        cluster-autoscaler.kubernetes.io/safe-to-evict="false"
 
     # we need to retrieve the latest docker image available for our EKS version
     K8S_VERSION=$(kubectl version --short | grep 'Server Version:' | sed 's/[^0-9.]*\([0-9.]*\).*/\1/' | cut -d. -f1,2)
@@ -113,73 +166,30 @@ EoF
         set image deployment.apps/cluster-autoscaler \
         cluster-autoscaler=us.gcr.io/k8s-artifacts-prod/autoscaling/cluster-autoscaler:v${AUTOSCALER_VERSION}
 
+    print_line
+
 }
 
 
 function install_metric_server()
 {
-    kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.5.1/components.yaml
-}
-
-
-function install_aws_loadbalancer_controller()
-{
-
-    curl -o iam_policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.3.0/docs/install/iam_policy.json
-    curl -o iam_policy_v1_to_v2_additional.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.3.0/docs/install/iam_policy_v1_to_v2_additional.json
-
-    aws iam create-policy \
-      --policy-name AWSLoadBalancerControllerIAMPolicy \
-      --policy-document file://iam_policy.json
-
-    aws iam create-policy \
-      --policy-name AWSLoadBalancerControllerAdditionalIAMPolicy \
-      --policy-document file://iam_policy_v1_to_v2_additional.json
-
-    eksctl create iamserviceaccount \
-      --cluster ${EKS_CLUSTER_NAME} \
-      --namespace kube-system \
-      --region $AWS_REGION \
-      --name aws-load-balancer-controller \
-      --attach-policy-arn "arn:aws:iam::${ACCOUNT_ID}:policy/AWSLoadBalancerControllerIAMPolicy" \
-      --override-existing-serviceaccounts \
-      --approve
-
-    myrole=$(eksctl get iamserviceaccount --cluster ${EKS_CLUSTER_NAME} --region ${AWS_REGION} --name aws-load-balancer-controller --namespace kube-system -o json | jq '.[].status.roleARN' | awk -F/ '{print $2}' |awk -F\" '{print $1}')
-
-    echo ${myrole}
-
-    aws iam attach-role-policy \
-      --role-name ${myrole} \
-      --policy-arn arn:aws:iam::${ACCOUNT_ID}:policy/AWSLoadBalancerControllerAdditionalIAMPolicy
-
-    kubectl apply -k github.com/aws/eks-charts/stable/aws-load-balancer-controller/crds?ref=master
-
-    kubectl get crd
-
-    helm repo add eks https://aws.github.io/eks-charts
-
-    helm install aws-load-balancer-controller \
-        eks/aws-load-balancer-controller \
-        -n kube-system \
-        --set clusterName=${EKS_CLUSTER_NAME} \
-        --set serviceAccount.create=false \
-        --set serviceAccount.name=aws-load-balancer-controller
-
-    kubectl -n kube-system rollout status deployment aws-load-balancer-controller
-
+    print_line
+    echo "Installing Metric Server"
+    print_line
     kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.5.1/components.yaml
     sleep 30
     kubectl get apiservice v1beta1.metrics.k8s.io -o json | jq '.status'
-
+    print_line
 }
 
-EKS_CFN_FILE=kubernetes_cluster.yaml
-EKS_STACK_NAME=auroragdbk8s
+export EKS_CFN_FILE=eks_cluster.yaml
+export EKS_STACK_NAME=auroragdbeks
+export INSTANCE_ROLE="C9Role"
+export EKS_NAMESPACE="kube-system"
 
-AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text) 
-EKS_CLUSTER_NAME=$(aws cloudformation describe-stacks --region ${AWS_REGION} --query "Stacks[].Outputs[?(OutputKey == 'EKSClusterName')][].{OutputValue:OutputValue}" --output text)
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text) 
+export EKS_CLUSTER_NAME=$(aws cloudformation describe-stacks --region ${AWS_REGION} --query "Stacks[].Outputs[?(OutputKey == 'EKSClusterName')][].{OutputValue:OutputValue}" --output text)
 vpcid=$(aws cloudformation describe-stacks --region ${AWS_REGION} --stack-name EKSGDB1 --query 'Stacks[].Outputs[?(OutputKey == `VPC`)][].{OutputValue:OutputValue}' --output text)
 subnets=$(aws ec2 describe-subnets --region ${AWS_REGION} --filters Name=vpc-id,Values=${vpcid} --query 'Subnets[?(! MapPublicIpOnLaunch)].{SubnetId:SubnetId}' --output text)
 
@@ -187,10 +197,10 @@ subnetA=`echo "${subnets}" |head -1 | tail -1`
 subnetB=`echo "${subnets}" |head -2 | tail -1`
 subnetC=`echo "${subnets}" |head -3 | tail -1`
 
-#create_eks_cluster
+chk_cloud9_permission
+create_eks_cluster
 update_config
 update_eks
-#install_loadbalancer
-#install_cluster_auto_scaler
-#install_metric_server
-install_aws_loadbalancer_controller
+install_loadbalancer
+install_cluster_auto_scaler
+install_metric_server
